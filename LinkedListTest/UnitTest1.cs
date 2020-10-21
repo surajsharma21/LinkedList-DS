de9783731c2f10c1ea917494b3baabc98031a641
Suraj <surajshrm000@gmail.com>
2020-10-21 22:38:45 +0530

    [Suraj] Add: Testing Insert Method

--- LinkedListTest/UnitTest1.cs
+++ LinkedListTest/UnitTest1.cs
@@ -7,13 +7,23 @@ namespace LinkedListTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSearch()
         {
             LinkedList list = new LinkedList();
             list.Add(12);
             list.Add(30);
             list.Add(56);
             Assert.IsTrue(list.Search(30));
+        }
+        [TestMethod]
+        public void TestInsert()
+        {
+            LinkedList list = new LinkedList();
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            list.Insert(3, 40);
+            Assert.IsTrue(list.Search(40));
         }
     }
 }
