2ff78b64b549ad05911da772920589341ffa051d
Suraj <surajshrm000@gmail.com>
2020-10-21 19:58:49 +0530

    [Suraj] Add: Creating a Simple Linked List

---
+++ LinkedList-DS/LinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_DS
+{
+    class LinkedList
+    {
+        internal Node head;
+        public LinkedList()
+        {
+            this.head = null;
+        }
+    }
+}
---
+++ LinkedList-DS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinkedList_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = new Node(56);
+            head.next = new Node(30);
+            head.next.next = new Node(70);
+        }
+    }
+}
