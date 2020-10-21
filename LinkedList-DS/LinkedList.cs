f7d9c1ca17ea3dec27ecf12c153b3c1e1139e39a
Suraj <surajshrm000@gmail.com>
2020-10-21 22:22:24 +0530

    [Suraj] Add: Remove Last Node in List

--- LinkedList-DS/LinkedList.cs
+++ LinkedList-DS/LinkedList.cs
@@ -82,6 +82,25 @@ namespace LinkedList_DS
                 return popped;
             }
         }
+        internal Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Empty LinkedList");
+                return null;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next.next != null)
+                {
+                    temp = temp.next;
+                }
+                Node removedNode = temp.next;
+                temp.next = null;
+                return removedNode;
+            }
+        }
         internal void Display()
         {
             if (head == null)
